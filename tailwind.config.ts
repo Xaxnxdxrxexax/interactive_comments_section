f355fae284d99e7c2ef3f87ef7d2b0bfad26b4e8
Andrea Sansone <andreasansone35@gmail.com>
2023-11-13 09:49:57 +0100

    added database

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        "Fm-Moderate-blue": "hsl(238, 40%, 52%)",
+        "Fm-Soft-Red": "hsl(358, 79%, 66%)",
+        "Fm-Light-grayish blue": "hsl(239, 57%, 85%)",
+        "Fm-Pale-red": "hsl(357, 100%, 86%)",
+        "Fm-Dark-blue": "hsl(212, 24%, 26%)",
+        "Fm-Grayish-Blue": "hsl(211, 10%, 45%)",
+        "Fm-Light-gray": "hsl(223, 19%, 93%)",
+        "Fm-Very-light-gray": "hsl(228, 33%, 97%)",
+        "Fm-White": "hsl(0, 0%, 100%)",
+      },
     },
   },
   plugins: [],

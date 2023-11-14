1bb397e9c052d90d672f3f16fa37a9e3c5be01ba
Andrea Sansone <andreasansone35@gmail.com>
2023-11-14 14:51:40 +0100

    listed comments with replies, plus some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,90 @@
 import Link from "next/link";
-import {
-  SignInButton,
-  SignOutButton,
-  UserButton,
-  auth,
-  currentUser,
-  useUser,
-} from "@clerk/nextjs";
+import { SignInButton, SignOutButton, UserButton, auth } from "@clerk/nextjs";
 
-import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
+import Image from "next/image";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 export default async function Home() {
-  // const hello = await api.post.hello.query({ text: "from tRPC" });
-
-  // const user = useUser();
   const { userId } = auth();
-
-  const user = await currentUser();
+  const allComments = await api.post.getAll.query();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      {userId ? <SignOutButton /> : <SignInButton />}
-      {userId && user?.username}
+    <main className="bg-Fm-Light-grayish blue mx-4 my-8">
+      {/* {userId ? <SignOutButton /> : <SignInButton />}
+      {userId && <UserButton />} */}
+      {allComments.map((post) => {
+        return (
+          <div key={post.id} className="relative bg-Fm-White p-4">
+            <div className="relative">
+              <div className="flex gap-4">
+                <div>
+                  <Image
+                    src={post.image}
+                    alt={`${post.username}'s picture`}
+                    width={20}
+                    height={20}
+                  />
+                </div>
+                <p>{post.username}</p>
+                <p>{dayjs(post.createdAt).fromNow()}</p>
+              </div>
+              <p>{post.content}</p>
+              <div className="flex">
+                <p>+</p>
+                <p>{post.score}</p>
+                <p>-</p>
+              </div>
+              <button className="absolute bottom-4 right-4 flex">
+                <Image
+                  src="/images/icon-reply.svg"
+                  alt="reply"
+                  width={20}
+                  height={20}
+                />
+                Reply
+              </button>
+            </div>
+            <div className="ml-4 border-l border-black pl-4">
+              {post.replies?.map((reply) => {
+                return (
+                  <div key={reply.id} className="relative bg-Fm-White p-4">
+                    <div className="flex gap-4">
+                      <div>
+                        <Image
+                          src={reply.image}
+                          alt={`${reply.username}'s picture`}
+                          width={20}
+                          height={20}
+                        />
+                      </div>
+                      <p>{reply.username}</p>
+                      <p>{dayjs(reply.createdAt).fromNow()}</p>
+                    </div>
+                    <p>{reply.content}</p>
+                    <div className="flex">
+                      <p>+</p>
+                      <p>{reply.score}</p>
+                      <p>-</p>
+                    </div>
+                    <button className="absolute bottom-4 right-4 flex">
+                      <Image
+                        src="/images/icon-reply.svg"
+                        alt="reply"
+                        width={20}
+                        height={20}
+                      />
+                      Reply
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
     </main>
   );
 }
-
-// async function CrudShowcase() {
-//   const latestPost = await api.post.getLatest.query();
-
-//   return (
-//     <div className="w-full max-w-xs">
-//       {latestPost ? (
-//         <p className="truncate">Your most recent post: {latestPost.name}</p>
-//       ) : (
-//         <p>You have no posts yet.</p>
-//       )}
-
-//       <CreatePost />
-//     </div>
-//   );
-// }
